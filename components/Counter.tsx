3b433a5725352a2252495c2859b7136794de7316
TaewoongHwang <taewoonghwang@lotte.net>
2022-12-12 17:56:50 +0900

    :hammer: feat(store): redux-toolkit 추가
    counter example을 활용한 boilerPlate 작성
    in redux-toolkit tutorials/quick-start

---
+++ components/Counter.tsx
@@ -0,0 +1,22 @@
+import type { RootState } from '../store/configureStore';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../store/counter/counterSlice';
+
+export function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
